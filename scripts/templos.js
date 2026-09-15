45c6a1306cd1ba5999caae1a3745fff586e35dda
gigiselima-creator <gigiselima@gmail.com>
2026-09-14 22:41:18 -0300

    Atualizando Album

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,12 +1,21 @@
-// Menu Hambúrguer Responsivo
 const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
-    hambutton.classList.toggle('open');
-});
+if (hambutton && mainnav) {
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('open');
+        hambutton.classList.toggle('open');
+    });
+}
 
-// Atualização dinâmica do Footer
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = `Última Modificação: ${document.lastModified}`;
+// Datas do Footer
+const currentYearElem = document.getElementById("currentyear");
+const lastModifiedElem = document.getElementById("lastModified");
+
+if (currentYearElem) {
+    currentYearElem.textContent = new Date().getFullYear();
+}
+
+if (lastModifiedElem) {
+    lastModifiedElem.textContent = `Última Modificação: ${document.lastModified}`;
+}
