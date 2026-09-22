33864b1814eb8096420b2819561d0145171d150e
gigiselima-creator <gigiselima@gmail.com>
2026-09-21 22:09:06 -0300

    correções da tarefa

--- scripts/local.js
+++ scripts/local.js
@@ -1,20 +1,20 @@
-// Atualização das informações do Rodapé
+// Atualização de ano e última modificação no rodapé
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Última modificação: ${document.lastModified}`;
 
-// Variáveis estáticas de temperatura (°C) e vento (km/h)
-const tempCelsius = 8;
-const windSpeedKmH = 10;
+// Obtenção dos valores estáticos do HTML
+const temp = parseFloat(document.getElementById("temp").textContent);
+const wind = parseFloat(document.getElementById("wind").textContent);
 
-// Função que calcula a sensação térmica (fórmula em sistema métrico)
+// Função de Sensação Térmica em 1 linha de retorno (Métrica)
 const calcularSensacaoTermica = (t, v) => 
   (13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16)).toFixed(1);
 
-// Verificação dos critérios para aplicação do cálculo
+// Validação dos critérios de cálculo
 const chillElement = document.getElementById("chill");
 
-if (tempCelsius <= 10 && windSpeedKmH > 4.8) {
-  chillElement.textContent = `${calcularSensacaoTermica(tempCelsius, windSpeedKmH)} °C`;
+if (temp <= 10 && wind > 4.8) {
+  chillElement.textContent = `${calcularSensacaoTermica(temp, wind)} °C`;
 } else {
   chillElement.textContent = "N/A";
 }
